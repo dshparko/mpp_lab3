010ed647825f2bc62aaa946537b141536f3effd5
Lenovo <d.shparko@mail.ru>
2021-11-26 19:48:28 +0300

    initial commit

---
+++ mpp_lab3/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace mpp_lab3
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ mpp_lab3/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace mpp_lab3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
