fb40c03d6d706ab7f8c8ab1ae4cd9995dc55c108
Lenovo <d.shparko@mail.ru>
2021-11-27 12:34:19 +0300

    add application interface

---
+++ AssemblyBrowserLib/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace AssemblyBrowserLib
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ mpp_lab3/ViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using Microsoft.Win32;
+
+namespace mpp_lab3
+{
+    public sealed class ModelView 
+    {
+
+    }
+
+}
